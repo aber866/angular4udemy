9249612889d954eb2ca9a6d206bf19e097cb7df8
Valverde-PC\Valverde <aber866@gmail.com>
2017-07-09 13:05:54 +0100

    directives

--- README.md
+++ README.md
@@ -11,4 +11,15 @@ ng generate component servers = ng g c servers // Genera un nuevo componente
 
 [(ngModel)]="name"
 selector: '[app-servers]', //si ponemos corchetes es que es un atributo y no un elemento
+							//dentro de @componentn en el js
 selector: ".app-servers", // selector by class
+
+ng-disabled="" ahora es [disabled]="!allowNewServer"
+[] //Property binding () //event binding --> (click)=""
+*ngIf="" //ng if
+[ngStyle]="{backgroundColor: getColor()}" //Nueva para estilos
+[ngClass]="{online: serverStatus === 'online'}" //ngclass
+
+directivas con * es que cambian el dom
+
+*ngFor="let server of servers" // creo que ngrepeat
--- project-template/src/app/servers/servers.component.ts
+++ project-template/src/app/servers/servers.component.ts
@@ -4,17 +4,36 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   //selector: ".app-servers",
-  template: `
+  /*template: `
   	<app-server></app-server>
   	<app-server></app-server>
-  `,
+  `,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = "No server was created";
+    serverName = 'Testserver';
+    serverCreated = false;
+    servers = ['Test server', 'Test server 2'];
 
-  constructor() { }
+    constructor() { 
+        setTimeout(() => {
+            this.allowNewServer = true;
+        },2000);
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onCreateServer() {
+        this.serverCreated = true;
+        this.servers.push(this.serverName);
+        this.serverCreationStatus = "Server was created! Name is" + this.serverName;
+    }
+    onUpdateServerElement(event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 
 }
